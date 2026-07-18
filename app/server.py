94b444cd027a8c95e187677879dede7d0fddfb3d
chandanch <chandan.cec107@gmail.com>
2026-07-18 11:17:45 +0530

    update server

--- app/server.py
+++ app/server.py
@@ -1,11 +1,16 @@
+from dotenv import load_dotenv
 from fastmcp import FastMCP
-from app.tools.rooms import register_room_tools
+
 from app.tools.bookings import register_booking_tools
+from app.tools.rooms import register_room_tools
+
+load_dotenv()
 
 mcp = FastMCP("room-booking-mcp")
 
 register_room_tools(mcp)
 register_booking_tools(mcp)
+
 
 if __name__ == "__main__":
     mcp.run()
